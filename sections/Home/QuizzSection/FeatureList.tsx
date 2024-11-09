c30e1a0791465e096755bed5c10ddba6aa15a40b
Raed-Khwayreh <raedkh.2000@gmail.com>
2024-11-09 16:17:09 +0200

    fix: font and props issues

--- sections/Home/QuizzSection/FeatureList.tsx
+++ sections/Home/QuizzSection/FeatureList.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./QuizzSection.module.css";
-import Img from "next/image";
+import Img, { StaticImageData } from "next/image";
 
-const QuizzSection = ({ title, description, icon }) => (
+interface QuizzSectionProps {
+  title: string;
+  description: string;
+  icon: StaticImageData;
+}
+
+const QuizzSection = ({ title, description, icon }: QuizzSectionProps) => (
   <div className={`${styles.section}`}>
     <div className={`${styles.iconHeading}`}>
       <h3 className={`${styles.heading}`}>{title}</h3>

4651ebed3d2d1da0b14db523fd0fc8002769be19
Alaa Al Mohamad <alamoh282@gmail.com>
2024-11-14 14:22:06 +0200

    update tablet breakpoint

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,10 @@ import React from "react";
 import InteractiveTools from "../sections/Home/InteractiveTools/InteractiveTools";
 import LearningPath from "../sections/Home/LearningPath/LearningPath";
 import QuizzSection from "../sections/Home/QuizzSection/QuizzSection";
-import ResourceCard from "@/components/ui/ResourceCard/ResourceCard";
-import cardData from "@/components/ui/ResourceCard/mocks/CardData";
 
 const page = () => {
   return (
     <div>
-       {cardData.map((item, index) => (
-          <ResourceCard
-            key={index}
-            title={item.title}
-            icon={item.icon}
-          />
-        ))}
-      <div>
-
-
-    </div>
       <QuizzSection />
       <LearningPath />
       <InteractiveTools />

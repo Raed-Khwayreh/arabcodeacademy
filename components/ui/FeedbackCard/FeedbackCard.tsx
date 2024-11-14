af0c8be2c36119a35e0b291fd49c6e2a0ef4dc41
Alaa Al Mohamad <alamoh282@gmail.com>
2024-11-14 02:10:33 +0200

    task/feedback component

---
+++ components/ui/FeedbackCard/FeedbackCard.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Image, { StaticImageData } from "next/image";
+import styles from "./FeedbackCard.module.css";
+import FilledStar from "./Icons/FilledStar";
+import OutlinedStar from "./Icons/OutlinedStar";
+import headerBackground from "./Icons/headerBackground.png";
+
+interface Props {
+  name: string;
+  image: string | StaticImageData;
+  rating: number;
+  comment: string;
+  date: string;
+}
+
+const FeedbackCardComponent: React.FC<Props> = ({
+  name,
+  image,
+  rating,
+  comment,
+  date,
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <Image
+          src={headerBackground}
+          alt="Header background"
+          className={styles.headerBackground}
+        />
+        <div className={styles.profileImageContainer}>
+          <Image
+            src={image}
+            alt={`${name}'s profile`}
+            className={styles.profileImage}
+          />
+        </div>
+      </div>
+      <div className={styles.name}>{name}</div>
+      <p className={styles.comment}>{comment}</p>
+      <div className={styles.dateContainer}>{date}</div>
+      <div className={styles.ratingContainer}>
+        {Array.from({ length: 5 }, (_, i) =>
+          i < rating ? <FilledStar key={i} /> : <OutlinedStar key={i} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackCardComponent;
---
+++ components/ui/FeedbackCard/FeedbackData.ts
@@ -0,0 +1,41 @@
+import user3Image from "@/components/ui/FeedbackCard/AvatarImages/user3.png";
+import user2Image from "@/components/ui/FeedbackCard/AvatarImages/user2.png";
+import user1Image from "@/components/ui/FeedbackCard/AvatarImages/user1.png"; // Add paths for other images as needed
+
+import { StaticImageData } from "next/image";
+
+interface Feedback {
+  name: string;
+  image: string | StaticImageData;
+  rating: number;
+  comment: string;
+  date: string;
+}
+
+export const feedbackData: Feedback[] = [
+  {
+    name: "اسم المستخدم",
+    image: user1Image,
+    rating: 4,
+    comment:
+      "تقدم الأكاديمية العربية للبرمجة تجربة تعلم متميزة من خلال مجموعة من الدروس والمناهج الاحترافية بجودة عالية وأسلوب تدريسي ممتع يتناسب",
+    date: "2023, 11 نيسان",
+  },
+
+  {
+    name: "اسم المستخدم",
+    image: user2Image,
+    rating: 4,
+    comment:
+      "تقدم الأكاديمية العربية للبرمجة تجربة تعلم متميزة من خلال مجموعة من الدروس والمناهج الاحترافية بجودة عالية وأسلوب تدريسي ممتع يتناسب.",
+    date: "2023, 11 نيسان",
+  },
+  {
+    name: "اسم المستخدم",
+    image: user3Image,
+    rating: 4,
+    comment:
+      "تقدم الأكاديمية العربية للبرمجة تجربة تعلم متميزة من خلال مجموعة من الدروس والمناهج الاحترافية بجودة عالية وأسلوب تدريسي ممتع يتناسب",
+    date: "2023, 11 نيسان",
+  },
+];

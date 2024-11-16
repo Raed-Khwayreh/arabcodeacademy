a60b53aef293dcc0879239e55f7b98130c6071b8
Dev-Ibrahim2019 <ialashqar.dev64@gmail.com>
2024-11-16 13:13:43 +0200

    fix shadow for ACButton component

--- components/ui/ACAButton/ACAButton.tsx
+++ components/ui/ACAButton/ACAButton.tsx
@@ -4,15 +4,22 @@ import styles from "./ACAButton.module.css";
 interface Props {
   text: string;
   variant: "primary" | "secondary" | "teal" | "tomato" | "transparent";
-  size: "small" | "medium" | "large" | "xlarge" ;
+  size: "small" | "medium" | "large" | "xlarge";
   icon?: React.ReactNode;
+  boxShadow?: string;
 }
 
-const ACAButton: React.FC<Props> = ({ text, variant, size, icon }) => {
+const ACAButton: React.FC<Props> = ({
+  text,
+  variant,
+  size,
+  icon,
+  boxShadow,
+}) => {
   const buttonClass = `${styles.button} ${styles[variant]} ${styles[size]}`;
 
   return (
-    <button className={buttonClass}>
+    <button className={buttonClass} style={{ boxShadow }}>
       {text}
       <span className={styles.icon}>{icon}</span>
     </button>

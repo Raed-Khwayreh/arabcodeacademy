e37c6859c4f3f35401d1759ac2c03cdef261fbcb
Raed-Khwayreh <raedkh.2000@gmail.com>
2024-12-01 13:23:23 +0200

    fix: sidebar menu

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -15,64 +15,71 @@ const Navbar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const handleOnlogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleOnLogOut = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <>
-      <div className={styles.navbar}>
-        <div className={styles["burger-menu"]} onClick={toggleSidebar}>
-          {isLoggedIn && (
-            <div className={styles["logout-mobile"]}>
-              <div onClick={() => setIsLoggedIn(false)}>
-                <Logout />
-              </div>
-              <Avatar />
-            </div>
-          )}
-          <BurgerMenu />
-        </div>
-        {isLoggedIn ? (
-          <div className={styles.logout}>
-            <div onClick={() => setIsLoggedIn(false)}>
+    <div className={styles.navbar}>
+      {isSidebarOpen && (
+        <Sidebar isLoggedIn={isLoggedIn} onLogin={handleOnlogin} />
+      )}
+      <div className={styles["burger-menu"]} onClick={toggleSidebar}>
+        {isLoggedIn && (
+          <div className={styles["logout-mobile"]}>
+            <div onClick={handleOnLogOut}>
               <Logout />
             </div>
             <Avatar />
           </div>
-        ) : (
-          <div className={styles["buttons-container"]}>
+        )}
+        <BurgerMenu />
+      </div>
+      {isLoggedIn ? (
+        <div className={styles.logout}>
+          <div onClick={handleOnLogOut}>
+            <Logout />
+          </div>
+          <Avatar />
+        </div>
+      ) : (
+        <div className={styles["buttons-container"]}>
+          <ACAButton
+            size="medium"
+            variant="teal"
+            text="إنشاء حساب"
+            icon={<ProfileCircleIcon />}
+          />
+          <div onClick={handleOnlogin}>
             <ACAButton
               size="medium"
-              variant="teal"
-              text="إنشاء حساب"
-              icon={<ProfileCircleIcon />}
+              variant="tomato"
+              text="تسجيل الدخول"
+              icon={<LoginIcon />}
             />
-            <div onClick={() => setIsLoggedIn(true)}>
-              <ACAButton
-                size="medium"
-                variant="tomato"
-                text="تسجيل الدخول"
-                icon={<LoginIcon />}
-              />
-            </div>
           </div>
-        )}
-        <ul className={styles.links}>
-          <li>المسارات التعليمية</li>
-          <li>التواصل </li>
-          <div className={styles["resource-list"]}>
-            <li>المصادر</li>
-            <ArrowDown />
-          </div>
-        </ul>
-        <Image
-          className={styles.image}
-          src={"/images/logo.png"}
-          alt="logo"
-          width={280}
-          height={61.79}
-        />
-      </div>
-
-      {isSidebarOpen && <Sidebar isLoggedIn={isLoggedIn} />}
-    </>
+        </div>
+      )}
+      <ul className={styles.links}>
+        <li>المسارات التعليمية</li>
+        <li>التواصل </li>
+        <div className={styles["resource-list"]}>
+          <li>المصادر</li>
+          <ArrowDown />
+        </div>
+      </ul>
+      <Image
+        className={styles.image}
+        src={"/images/logo.png"}
+        alt="logo"
+        width={280}
+        height={61.79}
+      />
+    </div>
   );
 };
 

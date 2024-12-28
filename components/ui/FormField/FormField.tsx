3534e63d0fc620b3d5fc10b3537ebba36e62486c
Alaa Al Mohamad <alamoh282@gmail.com>
2024-12-29 00:00:57 +0200

    update form field component props and css

--- components/ui/FormField/FormField.tsx
+++ components/ui/FormField/FormField.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./FormField.module.css";
+import AngleDownIcon from "../ACAButton/ACAButtonIcons/AngleDownIcon";
 
 interface Props {
   label: string;
@@ -7,23 +8,55 @@ interface Props {
   icon: React.ReactNode;
   name: string;
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void; 
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
+  isDropDown?: boolean;
+  options?: string[];
+  width?: string;
 }
 
-const FormField: React.FC<Props> = ({ label, placeholder, icon, name, value, onChange }) => {
+const FormField: React.FC<Props> = ({
+  label,
+  placeholder,
+  icon,
+  name,
+  value,
+  onChange,
+  isDropDown = false,
+  options = [],
+  width,
+}) => {
   return (
-    <div className={styles.fieldContainer}>
+    <div className={styles.fieldContainer} style={{ width }}>
       <div className={styles.labelContainer}>
         <label className={styles.formLabel}>{label}</label>
         <span>{icon}</span>
       </div>
-      <input
-        className={styles.formInput}
-        placeholder={placeholder}
-        name={name} 
-        value={value}
-        onChange={onChange} 
-      />
+      {isDropDown ? (
+        <div className={styles.selectInput}>
+          <select name={name} value={value} onChange={onChange}>
+            <option value="" disabled>
+              {placeholder}
+            </option>
+            {options.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <div className={styles.divider}></div> 
+          <AngleDownIcon />
+        </div>
+      ) : (
+        <input
+          className={styles.formInput}
+          placeholder={placeholder}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+      )}
     </div>
   );
 };

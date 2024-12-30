ba45baf4b64523f78211b5f709b1ae143777eac6
Raed-Khwayreh <raedkh.2000@gmail.com>
2024-12-30 21:19:06 +0200

    fix: update hero and resources sildres

--- sections/Home/ResourcesCard/ResourcesCardSection.tsx
+++ sections/Home/ResourcesCard/ResourcesCardSection.tsx
@@ -2,52 +2,48 @@
 import CarouselSlider from "@/components/ui/CarouselSlider/CarouselSlider";
 import cardData from "@/components/ui/ResourceCard/mocks/CardData";
 import ResourceCard from "@/components/ui/ResourceCard/ResourceCard";
-import {
-  LargeScreenSize,
-  MediumScreenSize,
-  SmallScreenSize,
-} from "@/constants/ScreenSizes";
-import useScreenSize from "@/utils/useScreenSize";
-import React, { useRef } from "react";
-import Slider from "react-slick";
+import { LargeScreenSize, MediumScreenSize } from "@/constants/ScreenSizes";
+import React from "react";
 import styles from "./ResourcesCardSection.module.css";
 
 const ResourcesCardSection: React.FC = () => {
-  const resourcesCardRef = useRef<Slider>(null);
-  const screenSize = useScreenSize();
+  const settings = {
+    dots: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: LargeScreenSize,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: MediumScreenSize,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <div className={styles.main}>
-      <CarouselSlider
-        containerBoxShadow={true}
-        buttonsPostion={{ desktop: 69, mobile: 32, tablet: 158 }}
-        breakPoints={{
-          desktop: { slidesToScroll: 3, slidesToShow: 3 },
-          tablet: {
-            screenSize: LargeScreenSize,
-            slidesToScroll: 2,
-            slidesToShow: 2,
-          },
-          mobile: {
-            screenSize: MediumScreenSize,
-            slidesToScroll: 1,
-            slidesToShow: 1,
-          },
-        }}
-        carouselContainerWidth={
-          screenSize < SmallScreenSize
-            ? 204
-            : screenSize < MediumScreenSize
-            ? 300
-            : screenSize < LargeScreenSize
-            ? 720
-            : 900
-        }
-        sliderRef={resourcesCardRef}
-        generatedSliderList={cardData.map((e, i) => (
-          <ResourceCard key={i} icon={e.icon} title={e.title} href={e.href} />
-        ))}
-      />
+      <div className={styles["slider-container"]}>
+        <CarouselSlider
+          buttonsPostion={{ desktop: 69, mobile: 32, tablet: 158 }}
+          userSettings={settings}
+          arrowsColor={"var(--primary-color)"}
+          generatedSliderList={cardData.map((e, i) => (
+            <ResourceCard key={i} icon={e.icon} title={e.title} href={e.href} />
+          ))}
+        />
+      </div>
     </div>
   );
 };
